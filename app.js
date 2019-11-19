e989bebe71ca9e58b8cf7bf74150365d1abb06eb
solemcaelum <tripyvox@gmail.com>
2019-11-19 04:34:38 -0600

    Incomplete commit so that I can sync with some Github Repo changes

--- app.js
+++ app.js
@@ -1,15 +1,109 @@
 const Discord = require('discord.js');
-const {token} = require("./config.json");
+const mongoose = require('mongoose');
+const {token, mongoConnect} = require("./config.json");
+mongoose.connect(mongoConnect, {useNewUrlParser: true, useUnifiedTopology: true}).catch(err => {
+  console.log("[🛰️  ] [❕] | First database connection!");
+});
 const client = new Discord.Client();
+const db = mongoose.connection;
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const userSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true
+  },
+  username: String,
+  discriminator: String,
+  avatar: String,
+  bot: Boolean,
+  data: Array
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+const guildSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true
+  },
+  owner_id: String,
+  permissions: Number,
+  analytics: {
+    features: Array,
+    mfa_level: Number,
+    large: Boolean,
+    unavailable: Boolean,
+    member_count: Number,
+    max_members: Number,
+    verification_level: Number,
+    default_message_notifications: Number,
+    explicit_content_filter: Number,
+    region: String,
+    roles: Array,
+    name: String,
+    icon: String,
+    members: Array,
+    channels: Array,
+    presences: Array
+  },
+  misc: {
+    splash: String,
+    vanity_url_code: String,
+    description: String,
+    banner: String,
+    premium_tier: Number,
+    emojis: Array
+  },
+  bot: {
+    changelog: Boolean,
+    changelogChannel: Number,
+    prefix: String,
+    firstTime: Boolean
+  },
+  logs: {
+    enabled: Boolean,
+    criticalPings: Boolean,
+    criticalRole: String,
+    logChannel: Number,
+    connect: Boolean,
+    disconnect: Boolean,
+    moderation: Boolean,
+    roles: Boolean,
+    settings: Boolean
   }
 });
 
+client.on('ready', () => {
+  console.log(`[🤖  ] | Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  console.log(`[🤖  ] | [${msg.guild.username}] <${msg.author.username}> : ${msg.content}!`);
+});
+
 client.login(token);
+
+db.once('open', function() {
+  console.log("[🛰️  ] [❕] | First database connection!");
+});
+
+db.on('connecting', function() {
+  console.log("[🛰️  ] [❕] | Attempting database connection!");
+});
+
+db.on('connected', function() {
+  console.log("[🛰️  ] [❕] | Database connected!");
+});
+
+db.on('reconnected', function() {
+  console.log("[🛰️  ] [❕] | Database reconnected!");
+});
+
+db.on('disconnected', function() {
+  console.log("[🛰️  ] [❕] | Database disconnected!");
+});
+
+process.on('SIGINT', function() {
+  console.log("[🖥️ ] [⚠️] | Recieved Shut Down command!")
+  console.log("[🖥️ ] [⚠️] | Gracefully shutting down.");
+  db.close();
+  process.exit();
+});
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Echleon is a work in progress.
 
-Please create a config.json with your "token".
+Please create a config.json with your "token" and "mongoConnect".
+Not that you should be doing anything yet.
+It's still being made.
+Be patient.
+Stay precious.
+<3
