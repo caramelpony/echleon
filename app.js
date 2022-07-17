77557047eb33ab0be42c06fdad4af985196d5560
Restyled.io <commits@restyled.io>
2022-07-17 04:38:09 +0000

    Restyled by prettier

--- app.js
+++ app.js
@@ -1,92 +1,105 @@
-const {Client, RichEmbed, Collection} = require('discord.js');
-const {getMember, warningEmbed, formatDate, updateBot, syncGuild, syncUser} =
-    require("./functions.js");
-require('dotenv').config();
-const mongoose = require('mongoose');
-const moment = require('moment');
-const findOrCreate = require('mongoose-findorcreate');
-const {prefix} = require("./config.json");
-const express = require('express');
+const { Client, RichEmbed, Collection } = require("discord.js");
+const {
+  getMember,
+  warningEmbed,
+  formatDate,
+  updateBot,
+  syncGuild,
+  syncUser,
+} = require("./functions.js");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const moment = require("moment");
+const findOrCreate = require("mongoose-findorcreate");
+const { prefix } = require("./config.json");
+const express = require("express");
 const port = 3000;
 
-mongoose.connect(process.env.MONGO_CONNECT)
-    .catch(e => { console.log("[🛰️  ] [❕] | " + e); });
+mongoose.connect(process.env.MONGO_CONNECT).catch((e) => {
+  console.log("[🛰️  ] [❕] | " + e);
+});
 
-const fs = require('fs');
+const fs = require("fs");
 
 const client = new Client();
 client.commands = new Collection();
 client.aliases = new Collection();
-client.categories = fs.readdirSync('./commands/');
+client.categories = fs.readdirSync("./commands/");
 client.cooldowns = new Collection();
 const db = mongoose.connection;
 
 var connectedDB = false;
 
-const Owner = require('./models/owner');
-const User = require('./models/user');
-const Guild = require('./models/guild');
-const {stripIndents} = require('common-tags');
+const Owner = require("./models/owner");
+const User = require("./models/user");
+const Guild = require("./models/guild");
+const { stripIndents } = require("common-tags");
 
-["command"].forEach(handler => { require(`./handler/${handler}`)(client); });
+["command"].forEach((handler) => {
+  require(`./handler/${handler}`)(client);
+});
 
 function presentTOS(guild) {
-  const channel = guild.channels.find(channel => channel.name === "general");
-  ;
-  const roleColor = guild.me.displayHexColor === "#000000"
-                        ? "#ffffff"
-                        : guild.me.displayHexColor;
+  const channel = guild.channels.find((channel) => channel.name === "general");
+  const roleColor =
+    guild.me.displayHexColor === "#000000"
+      ? "#ffffff"
+      : guild.me.displayHexColor;
 
-  const embed =
-      new RichEmbed()
-          //.setColor(roleColor)
-          .setColor('#99e786')
-          .setTitle(stripIndents`Echleon - Discord Server Analytics`)
-          .setDescription(
-              stripIndents`Echleon is built with Analytics, and control in mind. It strives to give you the information and tools to fully manage and grow your servers.`)
-          .setTimestamp()
-          .setAuthor(client.user.username, client.user.displayAvatarURL)
-          .setThumbnail(client.user.displayAvatarURL)
-          .setFooter(
-              `${client.user.username} - ${Math.round(client.ping)}ms Latency`,
-              client.user.displayAvatarURL)
-          .addField(
-              'Terms:', stripIndents`Server, Channel, and Memeber information
+  const embed = new RichEmbed()
+    //.setColor(roleColor)
+    .setColor("#99e786")
+    .setTitle(stripIndents`Echleon - Discord Server Analytics`)
+    .setDescription(
+      stripIndents`Echleon is built with Analytics, and control in mind. It strives to give you the information and tools to fully manage and grow your servers.`
+    )
+    .setTimestamp()
+    .setAuthor(client.user.username, client.user.displayAvatarURL)
+    .setThumbnail(client.user.displayAvatarURL)
+    .setFooter(
+      `${client.user.username} - ${Math.round(client.ping)}ms Latency`,
+      client.user.displayAvatarURL
+    )
+    .addField(
+      "Terms:",
+      stripIndents`Server, Channel, and Memeber information
         is collected and processed to create analytical information.
         Most of this data is collected with privacy in mind, but we still want to
         confirm server administration's consent.`,
-              true)
+      true
+    )
 
-          .addField(
-              'Instructions:',
-              stripIndents`None needed! You consented to these terms by adding the bot to your server.
+    .addField(
+      "Instructions:",
+      stripIndents`None needed! You consented to these terms by adding the bot to your server.
         You can also use \`=owner\` to learn more about the developer.`,
-              true)
+      true
+    )
 
-          .addField(
-              'Website:',
-              stripIndents`**Github**: https://github.com/solemcaelum/echleon`)
+    .addField(
+      "Website:",
+      stripIndents`**Github**: https://github.com/solemcaelum/echleon`
+    );
   if (channel) {
     channel.send(embed);
     syncGuild(guild);
-  }
-  else {
+  } else {
     syncGuild(guild);
   }
 }
 
 async function updatePresence() {
   client.user
-      .setPresence({
-        game : {name : ` ${client.guilds.size} servers!`, type : 'WATCHING'},
-        status : 'online'
-      })
-      .then(console.log(`[🤖  ] | Activity updated.`))
-      .catch(console.error);
-};
+    .setPresence({
+      game: { name: ` ${client.guilds.size} servers!`, type: "WATCHING" },
+      status: "online",
+    })
+    .then(console.log(`[🤖  ] | Activity updated.`))
+    .catch(console.error);
+}
 
 async function update() {
-  await updateBot().then(function(res) {
+  await updateBot().then(function (res) {
     if (res == true) {
       console.log("\x1b[31m\x1b[0m", "Bot Update Detected!");
     } else {
@@ -97,40 +110,48 @@ async function update() {
 
 update();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`[🤖  ] | Logged in as ${client.user.tag}!`);
   updatePresence();
-  console.log(`[🤖  ] | Ready to serve on ${
-      client.guilds.size} servers, for ${client.users.size} users.`);
+  console.log(
+    `[🤖  ] | Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`
+  );
 
-  client.users.forEach(user => {
-    User.findOrCreate({id : user.id}, {username : user.username},
-                      async function(err, foundGuild, created) {
-                        if (created) {
-                          console.log(`New user added to database! ID: ${
-                              user.id} | Username: ${user.username}`);
-                          syncUser(user);
-                        } else {
-                          console.log(`User Exists! Updating database! ID: ${
-                              user.id} | Username: ${user.username}`);
-                          syncUser(user);
-                        }
-                      });
+  client.users.forEach((user) => {
+    User.findOrCreate(
+      { id: user.id },
+      { username: user.username },
+      async function (err, foundGuild, created) {
+        if (created) {
+          console.log(
+            `New user added to database! ID: ${user.id} | Username: ${user.username}`
+          );
+          syncUser(user);
+        } else {
+          console.log(
+            `User Exists! Updating database! ID: ${user.id} | Username: ${user.username}`
+          );
+          syncUser(user);
+        }
+      }
+    );
   });
 
-  client.guilds.forEach(guild => {
+  client.guilds.forEach((guild) => {
     Guild.findOrCreate(
-        {id : guild.id}, {name : guild.name},
-        async function(err, foundGuild, created) {
-          if (created) {
-            console.log(`[🤖  ] | New server Discovered! \"${guild.name}\".`);
-            presentTOS(guild)
-            syncGuild(guild)
-          } else {
-            console.log(`[🤖  ] | Re-Discovered server! \"${guild.name}\".`);
-            syncGuild(guild);
-          }
-        });
+      { id: guild.id },
+      { name: guild.name },
+      async function (err, foundGuild, created) {
+        if (created) {
+          console.log(`[🤖  ] | New server Discovered! \"${guild.name}\".`);
+          presentTOS(guild);
+          syncGuild(guild);
+        } else {
+          console.log(`[🤖  ] | Re-Discovered server! \"${guild.name}\".`);
+          syncGuild(guild);
+        }
+      }
+    );
   });
 });
 
@@ -138,116 +159,133 @@ client.on("error", (e) => console.error("[🤖  ] [❌] | " + e));
 
 client.on("warn", (e) => console.warn("[🤖  ] [⚠️] | " + e));
 
-client.on('message', async message => {
+client.on("message", async (message) => {
   client.emit("guildUnavailable", message.channel.guild);
-  if (message.author.bot)
-    return;
-  if (!message.guild)
-    return;
-  if (!message.content.startsWith(prefix))
-    return;
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
   if (!message.member)
     message.member = await message.guild.fetchMember(message);
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
 
-  if (cmd.length === 0)
-    return;
+  if (cmd.length === 0) return;
 
   let command = client.commands.get(cmd);
-  if (!command)
-    command = client.commands.get(client.aliases.get(cmd));
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-  if (command)
-    command.run(client, message, args);
+  if (command) command.run(client, message, args);
 });
 
 client.on("guildMemberAdd", (member) => {
-  console.log(`[🤖  ] | "${member.user.username}" has joined "${
-      member.guild.name}".`);
+  console.log(
+    `[🤖  ] | "${member.user.username}" has joined "${member.guild.name}".`
+  );
   // member.guild.channels.find(c => c.name ===
   // "welcome").send(`"${member.user.username}" has joined this server`);
 });
 
 client.on("guildMemberRemove", (member) => {
   console.log(
-      `[🤖  ] | "${member.user.username}" has left "${member.guild.name}".`);
+    `[🤖  ] | "${member.user.username}" has left "${member.guild.name}".`
+  );
   // member.guild.channels.find(c => c.name ===
   // "welcome").send(`"${member.user.username}" has joined this server`);
 });
 
 client.on("guildCreate", (guild) => {
   Guild.findOrCreate(
-      {id : guild.id}, {name : guild.name},
-      async function(err, foundGuild, created) {
-        if (created) {
-          console.log(`[🤖  ] | New server Discovered! "${guild.name}".`);
-          presentTOS(guild);
-          syncGuild(guild)
-          updatePresence();
-
-        } else {
-          console.log(`[🤖  ] | Re-Discovered server! "${guild.name}".`);
-          syncGuild(guild);
-          updatePresence();
-        }
-      });
+    { id: guild.id },
+    { name: guild.name },
+    async function (err, foundGuild, created) {
+      if (created) {
+        console.log(`[🤖  ] | New server Discovered! "${guild.name}".`);
+        presentTOS(guild);
+        syncGuild(guild);
+        updatePresence();
+      } else {
+        console.log(`[🤖  ] | Re-Discovered server! "${guild.name}".`);
+        syncGuild(guild);
+        updatePresence();
+      }
+    }
+  );
 });
 
-client.on('guildUpdate',
-          async (oldGuild, newGuild) => { syncGuild(newGuild); });
+client.on("guildUpdate", async (oldGuild, newGuild) => {
+  syncGuild(newGuild);
+});
 
-client.on('guildUnavailable', async (guild) => { syncGuild(guild); });
+client.on("guildUnavailable", async (guild) => {
+  syncGuild(guild);
+});
 
-client.on('channelCreate', async (channel) => { syncGuild(channel.guild); });
+client.on("channelCreate", async (channel) => {
+  syncGuild(channel.guild);
+});
 
-client.on('channelDelete', async (channel) => { syncGuild(channel.guild); });
+client.on("channelDelete", async (channel) => {
+  syncGuild(channel.guild);
+});
 
-client.on('channelUpdate',
-          async (oldChannel, newChannel) => { syncGuild(newChannel.guild); });
+client.on("channelUpdate", async (oldChannel, newChannel) => {
+  syncGuild(newChannel.guild);
+});
 
-client.on('emojiCreate', async (emoji) => { syncGuild(emoji.guild); });
+client.on("emojiCreate", async (emoji) => {
+  syncGuild(emoji.guild);
+});
 
-client.on('emojiDelete', async (emoji) => { syncGuild(emoji.guild); });
+client.on("emojiDelete", async (emoji) => {
+  syncGuild(emoji.guild);
+});
 
-client.on('emojiUpdate',
-          async (oldEmoji, newEmoji) => { syncGuild(newEmoji.guild); });
+client.on("emojiUpdate", async (oldEmoji, newEmoji) => {
+  syncGuild(newEmoji.guild);
+});
 
-client.on('roleCreate', async (role) => { syncGuild(role.guild); });
+client.on("roleCreate", async (role) => {
+  syncGuild(role.guild);
+});
 
-client.on('roleDelete', async (role) => { syncGuild(role.guild); });
+client.on("roleDelete", async (role) => {
+  syncGuild(role.guild);
+});
 
-client.on('roleUpdate',
-          async (oldRole, newRole) => { syncGuild(newRole.guild); });
+client.on("roleUpdate", async (oldRole, newRole) => {
+  syncGuild(newRole.guild);
+});
 
-client.on('disconnect', () => {console.log("[🤖  ] | Disconnected.")});
+client.on("disconnect", () => {
+  console.log("[🤖  ] | Disconnected.");
+});
 
-db.once('open', function() {
+db.once("open", function () {
   console.log("[🛰️  ] [✔️] | First database connection!");
 });
 
-db.on('connecting', function() {
+db.on("connecting", function () {
   console.log("[🛰️  ] [🕐] | Attempting database connection!");
 });
 
-db.on('connected', function() {
+db.on("connected", function () {
   console.log("[🛰️  ] [✔️] | Database connected!");
   connectedDB = true;
 });
 
-db.on('reconnected', function() {
+db.on("reconnected", function () {
   console.log("[🛰️  ] [✔️] | Database reconnected!");
   connectedDB = true;
 });
 
-db.on('disconnected', function() {
+db.on("disconnected", function () {
   console.log("[🛰️  ] [❌] | Database disconnected!");
   connectedDB = false;
 });
 
-process.on('SIGINT', function() {
-  console.log("[🖥️ ] [⚠️] | Recieved Shut Down command!")
+process.on("SIGINT", function () {
+  console.log("[🖥️ ] [⚠️] | Recieved Shut Down command!");
   console.log("[🖥️ ] [⚠️] | Gracefully shutting down.");
   client.destroy();
   db.close();
@@ -259,15 +297,16 @@ client.login(process.env.FILLYTOKEN);
 const app = express();
 console.log("[Express] | Starting Dashboard.");
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
-  console.log("[Express] | GET Request from:", req.ip)
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+  console.log("[Express] | GET Request from:", req.ip);
+});
 
-app.listen(port,
-           () => console.log(`[Express] | Started Dashboard on port ${port}!`))
+app.listen(port, () =>
+  console.log(`[Express] | Started Dashboard on port ${port}!`)
+);
 
-    /*
+/*
       let guildPrefix = prefix;
       if (message.author.bot) return;
       var foundGuild;
--- functions.js
+++ functions.js
@@ -1,15 +1,15 @@
-const {RichEmbed} = require('discord.js');
-const {stripIndents} = require('common-tags');
-const axios = require('axios');
-const {version} = require('./package.json');
-const moment = require('moment');
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const axios = require("axios");
+const { version } = require("./package.json");
+const moment = require("moment");
 
-const Owner = require('./models/owner.js');
-const User = require('./models/user.js');
-const Guild = require('./models/guild.js');
+const Owner = require("./models/owner.js");
+const User = require("./models/user.js");
+const Guild = require("./models/guild.js");
 
 module.exports = {
-  getMember : function(message, toFind = '') {
+  getMember: function (message, toFind = "") {
     toFind = toFind.toLowerCase();
 
     let target = message.guild.members.get(toFind);
@@ -18,47 +18,53 @@ module.exports = {
       target = message.mentions.members.first();
 
     if (!target && toFind) {
-      target = message.guild.members.find(
-          member => {return member.displayName.toLowerCase().includes(toFind) ||
-                            member.user.tag.toLowerCase().includes(toFind)});
+      target = message.guild.members.find((member) => {
+        return (
+          member.displayName.toLowerCase().includes(toFind) ||
+          member.user.tag.toLowerCase().includes(toFind)
+        );
+      });
     }
 
-    if (!target)
-      target = message.member;
+    if (!target) target = message.member;
 
     return target;
   },
 
-  getGuildDomain : function(message) {
+  getGuildDomain: function (message) {
     var startingString = message.guild.name;
-    var output = startingString.toLowerCase()
-                     .replace(/[^a-zA-Z0-9 ]/g, "")
-                     .split(/ +/g)
-                     .join("-");
+    var output = startingString
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9 ]/g, "")
+      .split(/ +/g)
+      .join("-");
     return output;
   },
 
-  getUserDomain : function(member) {
+  getUserDomain: function (member) {
     var startingName = member.user.username;
     var outputName = startingName.replace(/([^a-zA-Z0-9 ])/g, "");
     outputName = outputName.toLowerCase().split(/ +/g).join("-");
     return outputName;
   },
 
-  formatDate : function(
-      date) { return new Intl.DateTimeFormat('en-US').format(date) },
+  formatDate: function (date) {
+    return new Intl.DateTimeFormat("en-US").format(date);
+  },
 
-  warningEmbed : function(client, message) {
-    const embed = new RichEmbed().setColor("#ff8200").setDescription(
-        stripIndents`⚠ \t${message}`);
+  warningEmbed: function (client, message) {
+    const embed = new RichEmbed()
+      .setColor("#ff8200")
+      .setDescription(stripIndents`⚠ \t${message}`);
 
     return embed;
   },
 
-  updateBot : async function() {
+  updateBot: async function () {
     try {
       const response = await axios.get(
-          'https://raw.githubusercontent.com/solemcaelum/echleon/master/package.json');
+        "https://raw.githubusercontent.com/solemcaelum/echleon/master/package.json"
+      );
       console.log(`${response.data.version} : ${version}`);
       if (response.version > version) {
         return true;
@@ -70,14 +76,14 @@ module.exports = {
     }
   },
 
-  syncGuild : async function(guildObject) {
+  syncGuild: async function (guildObject) {
     function formatDate(date) {
-      return new Intl.DateTimeFormat('en-US').format(date)
+      return new Intl.DateTimeFormat("en-US").format(date);
     }
     let time = moment().unix();
-    foundGuild =
-        await Guild.findOne({id : guildObject.id})
-            .catch(err => { console.log("[🛰️  ] [❕] | " + err); });
+    foundGuild = await Guild.findOne({ id: guildObject.id }).catch((err) => {
+      console.log("[🛰️  ] [❕] | " + err);
+    });
     foundGuild.timeOfSnap = time;
     foundGuild.analytics.name = guildObject.name;
     foundGuild.analytics.icon = guildObject.iconURL;
@@ -85,60 +91,55 @@ module.exports = {
     foundGuild.analytics.member_count = guildObject.memberCount;
     foundGuild.analytics.unavailable = guildObject.available;
     foundGuild.analytics.members = [];
-    guildObject.members.forEach(member => {
+    guildObject.members.forEach((member) => {
       foundGuild.analytics.members.push({
-        id : member.id,
-        name : member.user.username,
-        nick : member.nick,
-        joined : formatDate(member.joinedAt)
-      });
-    })
-    foundGuild.analytics.channels = [];
-    guildObject.channels.forEach(channel => {
-      let parent = 0;
-      if (channel.parentID)
-        parent = channel.parentID;
-      let topic = "";
-      if (channel.topic)
-        topic = channel.topic;
-      let nsfw = false;
-      if (channel.nsfw)
-        nsfw = channel.nsfw;
-      foundGuild.analytics.channels.push({
-        id : Number(channel.id),
-        name : channel.name,
-        chantype : channel.type,
-        position : channel.position,
-        nsfw : nsfw,
-        topic : topic,
-        parent : parent
+        id: member.id,
+        name: member.user.username,
+        nick: member.nick,
+        joined: formatDate(member.joinedAt),
       });
     });
-    foundGuild.save(err => {
-      if (err)
-        console.log(err);
+    foundGuild.analytics.channels = [];
+    guildObject.channels.forEach((channel) => {
+      let parent = 0;
+      if (channel.parentID) parent = channel.parentID;
+      let topic = "";
+      if (channel.topic) topic = channel.topic;
+      let nsfw = false;
+      if (channel.nsfw) nsfw = channel.nsfw;
+      foundGuild.analytics.channels.push({
+        id: Number(channel.id),
+        name: channel.name,
+        chantype: channel.type,
+        position: channel.position,
+        nsfw: nsfw,
+        topic: topic,
+        parent: parent,
+      });
+    });
+    foundGuild.save((err) => {
+      if (err) console.log(err);
     });
   },
 
-  syncUser : async function(userObject) {
+  syncUser: async function (userObject) {
     let time = moment().unix();
-    foundUser =
-        await User.findOne({id : userObject.id})
-            .catch(err => { console.log("[🛰️  ] [❕] | " + err); });
+    foundUser = await User.findOne({ id: userObject.id }).catch((err) => {
+      console.log("[🛰️  ] [❕] | " + err);
+    });
     foundUser.username = userObject.username;
     foundUser.discriminator = userObject.discriminator;
     foundUser.avatar = userObject.displayAvatarURL;
     foundUser.bot = userObject.bot;
-    foundUser.save(err => {
-      if (err)
-        console.log(err);
+    foundUser.save((err) => {
+      if (err) console.log(err);
     });
   },
 
-  checkUserData : async function(client, userObject, data) {
-    foundUser =
-        await User.findOne({id : userObject.id})
-            .catch(err => { console.log("[🛰️  ] [❕] | " + err); });
+  checkUserData: async function (client, userObject, data) {
+    foundUser = await User.findOne({ id: userObject.id }).catch((err) => {
+      console.log("[🛰️  ] [❕] | " + err);
+    });
     var i = 0;
     var userData = [];
     for (const badge of foundUser.data) {
@@ -146,6 +147,5 @@ module.exports = {
       i++;
     }
     return userData;
-  }
-
-}
+  },
+};
